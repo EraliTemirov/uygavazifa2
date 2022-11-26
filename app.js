e2947346e06fd1c1e5fba4e590f67f5cbda9e06f
Erali <eralitemirov129@gmail.com>
2022-11-26 11:50:56 +0500

    qwer

---
+++ app.js
@@ -0,0 +1,10 @@
+for(; ;){
+    let i=+prompt("Son kiriting");
+    
+if(i%2===0){
+    alert(`${i} soni juft`);
+}else if(i%2===1){
+    alert(`${i} soni toq`);
+}else
+alert("Iltimos 0 dan 12 gacha bo'lgan son kiriting!");
+}
